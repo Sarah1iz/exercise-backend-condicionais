234bf2aecd91974b89292f6c3c5ead9d551218f8
sarah <sarah.rendeiro@outlook.com>
2023-01-18 20:30:57 -0300

    vôlei 1

--- 06/index.js
+++ 06/index.js
@@ -1,3 +1,19 @@
+// <180 = reprovado
+// >= 180 && <= 185 = líbero
+// >= 186 && <= 195 = ponteiro
+// >=196 && <=205 = oposto
+// >205 = central
 const alturaEmCm = 185;
 
-//seu código aqui
+if (alturaEmCm >= 180 && alturaEmCm <= 185) {
+    console.log("LÍBERO");
+} if (alturaEmCm >= 186 && alturaEmCm <= 195) {
+    console.log("PONTEIRO");
+} if (alturaEmCm >= 196 && alturaEmCm <= 205) {
+    console.log("OPOSTO");
+} if (alturaEmCm > 205) {
+    console.log("CENTRAL");
+} else if (alturaEmCm < 180) {
+    console.log("REPROVADO")
+}
+
