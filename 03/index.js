13a84f83ddbcdd41d74efa5e78f66f4f9601f70b
sarah <sarah.rendeiro@outlook.com>
2023-01-18 19:37:37 -0300

    dominó

--- 03/index.js
+++ 03/index.js
@@ -1,4 +1,12 @@
+// 1 pedra = 2 numeros
+//bucha = mesmo numero nos 2 lados
+//sim = igual / não = diferente
+
 const ladoA = 3;
 const ladoB = 3;
 
-//seu código aqui
+if (ladoA === ladoB) {
+    console.log("SIM");
+} if (ladoA !== ladoB) {
+    console.log("NÃO");
+}
