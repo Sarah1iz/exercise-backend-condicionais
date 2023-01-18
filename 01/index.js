68ebf248c0cb742e8f504855343fa3ccc8db9082
sarah <sarah.rendeiro@outlook.com>
2023-01-18 18:58:03 -0300

    pedra, papel e tesoura

--- 01/index.js
+++ 01/index.js
@@ -1,4 +1,16 @@
 const jogada1 = "pedra"
 const jogada2 = "tesoura"
+//aqui imprime PEDRA
+//imprimir o nome da jogada vencedora ou empate
+//pedra,papel ou tesoura
 
-//seu código aqui
+if (jogada1 === "pedra" && jogada2 === "tesoura" || jogada1 === "tesoura" && jogada2 === "pedra") {
+    console.log("pedra");
+} if (jogada1 === "pedra" && jogada2 === "papel" || jogada1 === "papel" && jogada2 === "pedra") {
+    console.log("papel");
+} if (jogada1 === "tesoura" && jogada2 === "papel" || jogada1 === "papel" && jogada2 === "tesoura") {
+    console.log("tesoura");
+} else if (jogada1 === jogada2) {
+    console.log("empate");
+}
+
