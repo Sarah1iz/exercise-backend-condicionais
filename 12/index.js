efc2d04d4a7cb395775768b081c8ba9cbdd07569
sarah <sarah.rendeiro@outlook.com>
2023-01-19 08:35:32 -0300

    dia da semana

--- 12/index.js
+++ 12/index.js
@@ -1,1 +1,20 @@
-const diaDaSemana = 3;
+// semana = 7 dias (segunda=1/domingo=7)
+const diaDaSemana = 1;
+
+if (diaDaSemana == 1) {
+    console.log("SEGUNDA FEIRA");
+} if (diaDaSemana == 2) {
+    console.log("TERÇA FEIRA");
+} if (diaDaSemana == 3) {
+    console.log("QUARTA FEIRA");
+} if (diaDaSemana == 4) {
+    console.log("QUINTA FEIRA");
+} if (diaDaSemana == 5) {
+    console.log("SEXTA FEIRA");
+} if (diaDaSemana == 6) {
+    console.log("SÁBADO");
+} if (diaDaSemana == 7) {
+    console.log("DOMINGO");
+} if (diaDaSemana > 7) {
+    console.log("O dia da semana informado não é válido.");
+}
