841342269b290a610da0630647a200fa62e7c1ae
sarah <sarah.rendeiro@outlook.com>
2023-01-19 09:51:42 -0300

    compra com desconto

--- 13/index.js
+++ 13/index.js
@@ -1,6 +1,19 @@
+//credito= 5; cheque= 3; debito ou dinheiro= 10
+// valor final = valoedoproduto - (valordoproduto*desconto)
+
 //tipo de pagamento (dinheiro, credito, debito, cheque).
 const tipoDePagamento = "credito";
 
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+if (tipoDePagamento == "credito") {
+    let valorFinal = (valorDoProduto - (valorDoProduto * 0.05)) / 100
+    console.log(`Valor a ser pago: R$ ${valorFinal.toFixed(2)}`)
+} else if (tipoDePagamento == "cheque") {
+    let valorFinal = (valorDoProduto - (valorDoProduto * 0.03)) / 100
+    console.log(`Valor a ser pago: R$ ${valorFinal.toFixed(2)}`)
+} else if (tipoDePagamento == "debito" || tipoDePagamento == "dinheiro") {
+    let valorFinal = (valorDoProduto - (valorDoProduto * 0.10)) / 100
+    console.log(`Valor a ser pago: R$ ${valorFinal.toFixed(2)}`)
+}
