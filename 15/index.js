db0140a31f202536a9e1b38f4f994484314a1028
sarah <sarah.rendeiro@outlook.com>
2023-01-19 10:34:14 -0300

    Nome para exibição

--- 15/index.js
+++ 15/index.js
@@ -1,4 +1,18 @@
+//apenas o apelido(opcional) || primeiro nome + sobrenome(opcional) || apenas o primeiro nome(obrogatorio)
+// USAR THUTHINESS
+
 const primeiroNome = "Mario";
 const sobrenome = "";
 const apelido = "";
 
+if (primeiroNome && sobrenome && apelido) {
+    console.log(apelido);
+} if (primeiroNome && sobrenome && !apelido) {
+    console.log(`${primeiroNome} ${sobrenome}`);
+} if (primeiroNome && !sobrenome && !apelido) {
+    console.log(primeiroNome);
+} else if (primeiroNome && !sobrenome && apelido) {
+    console.log(apelido);
+} else {
+    console.log("Insira um nome")
+}
