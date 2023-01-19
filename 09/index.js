e0e1ebc1ac9c444fb79a4d218d7f28c24cb73cd3
sarah <sarah.rendeiro@outlook.com>
2023-01-19 07:23:36 -0300

    notas da escola

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,15 @@
+// notas de 0 a 10
+// conceito de A até E
 const nota = 8.5;
+
+if (nota < 4) {
+    console.log("O estudando obteve conceito E");
+} if (nota >= 4 && nota <= 5.9) {
+    console.log("O estudando obteve conceito D");
+} if (nota >= 6 && nota <= 7.9) {
+    console.log("O estudando obteve conceito C");
+} if (nota >= 8 && nota <= 8.9) {
+    console.log("O estudando obteve conceito B");
+} if (nota >= 9 && nota <= 10) {
+    console.log("O estudando obteve conceito A");
+}
