af94981f21c7bdb702d58dc28ca1cfb4b5ddf50d
sarah <sarah.rendeiro@outlook.com>
2023-01-18 23:24:22 -0300

    isenção de imposto

--- 07/index.js
+++ 07/index.js
@@ -1,5 +1,15 @@
+// isentos= portador de doença, aposentados = 
+// vaza = rendimento < 28_599.70
+// paga = rendimento > 28_599.70 
+
 const aposentada = false;
 const portadoraDeDoenca = false;
 const totalDeRendimentos = 3000000; //emCentavos
 
-//seu código aqui
+if (aposentada && portadoraDeDoenca || aposentada || portadoraDeDoenca) {
+    console.log("ISENTA");
+} if (!aposentada && !portadoraDeDoenca && totalDeRendimentos < 28_599.70) {
+    console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE");
+} else if (!aposentada && !portadoraDeDoenca && totalDeRendimentos > 28_599.70) {
+    console.log("PEGA LEAO");
+}                                   
