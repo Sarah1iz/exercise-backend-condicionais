316f417d2f2f5c3daad3a5bc571b205bd34870c0
sarah <sarah.rendeiro@outlook.com>
2023-01-19 09:30:44 -0300

    caractere

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,18 @@
+let vogalMaiuscula = ["A", "E", "I", "O", "U"];
+let vogalMinuscula = ["a", "e", "i", "o", "u"];
+let numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+
 const caractere = "E";
+
+
+if (numero.includes(caractere)) {
+    console.log("número");
+} else if (vogalMaiuscula.includes(caractere)) {
+    console.log("vogal maiúscula");
+} else if (vogalMinuscula.includes(caractere)) {
+    console.log("vogal minúscula");
+} else {
+    console.log("consoante");
+}
+
